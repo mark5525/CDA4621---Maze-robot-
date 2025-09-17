e10d1bdd4b0538c0a94821ea4929ff88cba69231
mark5525 <halimmark5@gmail.com>
2025-09-16 20:30:28 -0400

    Lab 1.py

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -9,7 +9,8 @@ time.sleep(7)
 # 7  seconds is the end of the 4 *4 maze 
 Bot.set_left_motor_speed(50)
 Bot.set_right_motor_speed(-50)
-time.sleep(1.2)
+time.sleep(1.19)
+# 1.2 is perfect for turning 90 degrees 
 Bot.stop_motors()
 x= 1
 match(x):
