a110a9073358685c94d682cd56fa798e01c44578
mark5525 <halimmark5@gmail.com>
2025-09-16 20:23:44 -0400

    Lab 1.py

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -9,7 +9,7 @@ time.sleep(7)
 # 7  seconds is the end of the 4 *4 maze 
 Bot.set_left_motor_speed(50)
 Bot.set_right_motor_speed(-50)
-time.sleep(2)
+time.sleep(1.2)
 Bot.stop_motors()
 x= 1
 match(x):
