e1fa7a74d331d8cd52d74bb308544259840e3982
mark5525 <halimmark5@gmail.com>
2025-09-16 20:40:07 -0400

    Lab 1.py

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -19,10 +19,14 @@ if turn != 1.19:
 # one motor turns negative to turn it 
 # left positive and right negative makes it turn clockwise
 # 1.19 time is perfect for turning 90 degrees 
-Bot.set_left_motor_speed(50)  
-Bot.set_right_motor_speed(50) 
-time.sleep(7)
-Bot.stop_motors()
+turnaround = 0
+if turnaround != 7:
+    Bot.set_left_motor_speed(50)  
+    Bot.set_right_motor_speed(50) 
+    time.sleep(7)
+    Bot.stop_motors()
+
+
 match(x):
     case 1:
         print("hello world")
