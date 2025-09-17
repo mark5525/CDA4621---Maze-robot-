06121e898fdb02a5066002c98adb531c8c761367
mark5525 <halimmark5@gmail.com>
2025-09-16 20:19:49 -0400

    Lab 1.py

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -2,9 +2,14 @@ from robot_systems.robot import HamBot
 import time
 Bot = HamBot(lidar_enabled = False, camera_enabled = False)
 
-Bot.set_left_motor_speed(50)  
+'''Bot.set_left_motor_speed(50)  
 Bot.set_right_motor_speed(50) 
 time.sleep(7)
+'''
+# 7  seconds is the end of the 4 *4 maze 
+Bot.set_left_motor_speed(50)
+Bot.set_right_motor_speed(-50)
+time.sleep(2)
 Bot.stop_motors()
 x= 1
 match(x):
