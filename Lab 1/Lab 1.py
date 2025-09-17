a9ac8dfc8c1522f10106f318c994213d0ef492ce
mark5525 <halimmark5@gmail.com>
2025-09-16 20:07:58 -0400

    Lab 1.py

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -1,1 +1,12 @@
-print("hello world")
+from robot_systems.robot import HamBot
+import time
+Bot = HamBot(lidar_enabled = False, camera_enabled = False)
+
+Bot.set_left_motor_speed(50)  
+Bot.set_right_motor_speed(50) 
+time.sleep(2)
+Bot.stop_motors()
+x= 1
+match(x):
+    case 1:
+        print("hello world")
