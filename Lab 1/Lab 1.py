09a9f0ab26da9773424e92b69e051ff9a49c6c3f
mark5525 <halimmark5@gmail.com>
2025-09-18 16:57:41 -0400

    waypoints

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -1,45 +1,49 @@
 import sys
 from HamBot.src.robot_systems import HamBot
-sys.path.append
+#sys.path.append
 import time
 import math
-
+def StraightLineFormula(x1, x2, y1, y2):
+    #the starting point is x1, y1
+    math.sqrt(math.pow(x2-x1, 2) + math.pow(y2 - y1, 2) )
 # kinematics calculations
 # using 50 rpms
 AxisLength = 4
 WheelDiameter = 2.6 #in
 WheelRadius = WheelDiameter /2
 RPM = 50
-LeftWheelRadius = (RPM * 2 * math.pi) / 60
+LeftWheelRadius = 0
+RightWheelRadius = 0
+TotalPathLength = 0
+TotalTravelTime = 0
+#use time.time()  0r time.perf_counter for the travel time
+p0 = [2.0,-2.0,math.pi],
+p1 = [-1.5, -2.0, math.pi]
+p2 = [-2.0, -1.5, math.pi/2]
+p3 = [-2.0, -0.5, math.pi/2]
+p4 = [-1.0, -0.5, (3 * math.pi)/2]
+p5 = [-0.5, -1.0, (7 * math.pi)/4]
+p6 = [2.0, -1.0, 0]
+p7 = [2.0, 0.0, math.pi/2]
+P8 = [0.0, 0.0, math.pi]
+P9 = [0.0, 1.0, math.pi/2]
+P10 = [-2.0, 1.0, math.pi]
+P11 = [-1.0, 2.0, 0]
+P12 = [1.5, 2.0, 0]
+P13 = [0, 0, 0] #UNKOWN VALUE
 
 #robot movement
 Bot = HamBot(lidar_enabled = False, camera_enabled = False)
-end = 0
-if  end != 7:
-    Bot.set_left_motor_speed(50)  
-    Bot.set_right_motor_speed(50) 
-    time.sleep(6)
-    Bot.stop_motors()
-    end = 7
-# 7  seconds is the end of the 4 *4 maze 
-turn = 0
-if turn != 1.19:
-    Bot.set_left_motor_speed(50)
-    Bot.set_right_motor_speed(-50)
-    time.sleep(1.19)
-    Bot.stop_motors()
-    turn = 1.19
+
+P1toP2LeftWheelRadius =
+
+
+
+
+
+
+
+# 7  seconds is the end of the 4 *4 maze
 # one motor turns negative to turn it 
 # left positive and right negative makes it turn clockwise
-# 1.19 time is perfect for turning 90 degrees 
-turnaround = 0
-if turnaround != 7:
-    Bot.set_left_motor_speed(50)  
-    Bot.set_right_motor_speed(50) 
-    time.sleep(6)
-    Bot.stop_motors()
-
-x = 0
-match(x):
-    case 1:
-        print("hello world")
+# 1.19 time is perfect for turning 90 degrees
---
+++ Lab 1/testing code.py
@@ -0,0 +1,48 @@
+import sys
+from HamBot.src.robot_systems import HamBot
+sys.path.append
+import time
+import math
+
+# kinematics calculations
+# using 50 rpms
+AxisLength = 4
+WheelDiameter = 2.6 #in
+WheelRadius = WheelDiameter /2
+RPM = 50
+LeftWheelRadius = 0
+RightWheelRadius = 0
+TotalPathLength = 0
+TotalTravelTime = 0
+#use time.time()  0r time.perf_counter for the travel time
+#robot movement
+Bot = HamBot(lidar_enabled = False, camera_enabled = False)
+end = 0
+if  end != 7:
+    Bot.set_left_motor_speed(50)
+    Bot.set_right_motor_speed(50)
+    time.sleep(6)
+    Bot.stop_motors()
+    end = 7
+# 7  seconds is the end of the 4 *4 maze
+turn = 0
+if turn != 1.19:
+    Bot.set_left_motor_speed(50)
+    Bot.set_right_motor_speed(-50)
+    time.sleep(1.19)
+    Bot.stop_motors()
+    turn = 1.19
+# one motor turns negative to turn it
+# left positive and right negative makes it turn clockwise
+# 1.19 time is perfect for turning 90 degrees
+turnaround = 0
+if turnaround != 7:
+    Bot.set_left_motor_speed(50)
+    Bot.set_right_motor_speed(50)
+    time.sleep(6)
+    Bot.stop_motors()
+
+x = 0
+match(x):
+    case 1:
+        print("hello world")
