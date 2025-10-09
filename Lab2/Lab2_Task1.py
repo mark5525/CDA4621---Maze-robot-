21cbaae9fa46babccf5a3e84404747102f1f0c3a
dschagen <dschagen6@gmail.com>
2025-10-09 17:44:37 -0400

    Rewrote while loop

--- Lab2/Lab2_Task1.py
+++ Lab2/Lab2_Task1.py
@@ -27,12 +27,12 @@ def side_PID(Bot, side_distance = 0.1, kp = 3, side = "left"):
 if __name__ == "__main__":
     Bot = HamBot(lidar_enabled=True, camera_enabled=False)
     scan = Bot.get_range_image()
-    forward_distance = min(scan[180])
-    forward_velocity = forward_PID(kp=3)
-    delta_velocity = side_PID(kp=0.1)
-    side_distance = min(scan[90])
 
-    while min(scan[180]) != forward_distance:
+    while min(scan[180]) != 0.6:
+        forward_distance = min(scan[180])
+        forward_velocity = forward_PID(kp=3)
+        delta_velocity = side_PID(kp=0.1)
+        side_distance = min(scan[90])
         # too close
         if side_distance < 2:
             left_v = forward_velocity
