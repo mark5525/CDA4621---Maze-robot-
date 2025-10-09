981c8b1114bd47156a9c779b9b8401c9582da4ea
dschagen <dschagen6@gmail.com>
2025-10-09 17:08:13 -0400

    Trying

--- Lab2/Lab2_Task1.py
+++ Lab2/Lab2_Task1.py
@@ -1,5 +1,4 @@
 from HamBot.src.robot_systems.robot import HamBot
-from HamBot.src.robot_systems.lidar import Lidar
 import math
 
 def saturation(robot,v):
@@ -26,24 +25,24 @@ def side_PID(Bot, side_distance = 0.1, kp = 3, side = "left"):
 if __name__ == "__main__":
     Bot = HamBot(lidar_enabled=True, camera_enabled=False)
 
-    while Bot.experiment_supervisor.step(Bot.timestep) != 1:
-        forward_distance = min(Bot.get_lidar_range_image()[175: 180])
-        forward_velocity = forward_PID(kp=3)
-        delta_velocity = side_PID(kp=0.1)
-        side_distance = abs(Bot.get_lidar_range_image()[90:115])
-        # too close
-        if side_distance < 2:
-            left_v = forward_velocity
-            right_v = saturation(forward_velocity - delta_velocity)
-        elif side_distance > 2:
-            right_v = forward_velocity
-            right_v = saturation(forward_velocity - delta_velocity)
-        else:
-            right_v = forward_velocity
-            left_v = forward_velocity
+    scan = Bot.get_range_image()
+    forward_distance = min(scan[180])
+    forward_velocity = forward_PID(kp=3)
+    delta_velocity = side_PID(kp=0.1)
+    side_distance = min(scan[90])
+    # too close
+    if side_distance < 2:
+        left_v = forward_velocity
+        right_v = saturation(forward_velocity - delta_velocity)
+    elif side_distance > 2:
+        right_v = forward_velocity
+        right_v = saturation(forward_velocity - delta_velocity)
+    else:
+        right_v = forward_velocity
+        left_v = forward_velocity
 
-        Bot.set_left_motor_velocity(left_v)
-        Bot.set_right_motor_velocity(right_v)
+    Bot.set_left_motor_velocity(left_v)
+    Bot.set_right_motor_velocity(right_v)
 
 
 
