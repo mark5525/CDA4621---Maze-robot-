cf3c6f8b6f280feb08921a4f22d99f450a035405
dschagen <dschagen6@gmail.com>
2025-10-09 17:40:48 -0400

    reordered main function and made a while loop to stop the robot at 0.6 m from wall

--- Lab2/Lab2_Task1.py
+++ Lab2/Lab2_Task1.py
@@ -26,25 +26,26 @@ def side_PID(Bot, side_distance = 0.1, kp = 3, side = "left"):
 
 if __name__ == "__main__":
     Bot = HamBot(lidar_enabled=True, camera_enabled=False)
-
     scan = Bot.get_range_image()
     forward_distance = min(scan[180])
     forward_velocity = forward_PID(kp=3)
     delta_velocity = side_PID(kp=0.1)
     side_distance = min(scan[90])
-    # too close
-    if side_distance < 2:
-        left_v = forward_velocity
-        right_v = saturation(forward_velocity - delta_velocity)
-    elif side_distance > 2:
-        right_v = forward_velocity
-        right_v = saturation(forward_velocity - delta_velocity)
-    else:
-        right_v = forward_velocity
-        left_v = forward_velocity
 
-    Bot.set_left_motor_velocity(left_v)
-    Bot.set_right_motor_velocity(right_v)
+    while min(scan[180]) != forward_distance:
+        # too close
+        if side_distance < 2:
+            left_v = forward_velocity
+            right_v = saturation(forward_velocity - delta_velocity)
+        elif side_distance > 2:
+            right_v = forward_velocity
+            right_v = saturation(forward_velocity - delta_velocity)
+        else:
+            right_v = forward_velocity
+            left_v = forward_velocity
+
+        Bot.set_left_motor_velocity(left_v)
+        Bot.set_right_motor_velocity(right_v)
 
 
 
