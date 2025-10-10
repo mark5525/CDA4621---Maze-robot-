a675760d3200a08127419dea6cb8eeae3263cd8f
dschagen <dschagen6@gmail.com>
2025-10-09 22:00:38 -0400

    added code to init.py

--- Lab 1/__init__.py
+++ Lab 1/__init__.py
@@ -1,3 +1,3 @@
 # make `from HamBot import HamBot` work
-from HamBot.src.robot import HamBot      # or from .src.robot_systems import HamBot
+from HamBot.src.robot_systems.robot import HamBot      # or from .src.robot_systems import HamBot
 __all__ = ["HamBot"]
