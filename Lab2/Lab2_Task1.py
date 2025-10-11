8be27d7d382f75759560390a2f56b376c307ebda
dschagen <dschagen6@gmail.com>
2025-10-10 23:38:49 -0400

    Recreated saturation function in RPMs, and reworked while loop and forward_PID function

--- Lab2/Lab2_Task1.py
+++ Lab2/Lab2_Task1.py
@@ -1,22 +1,33 @@
 from HamBot.src.robot_systems.robot import HamBot
 import math
 
-#def saturation(Bot,v):
 
-#def forward_PID(Bot, forward_distance = 0.6, kp = 3):
+def saturation(Bot,rpm):
+    max_rpm = 50
+    if rpm > max_rpm:
+        return max_rpm
+    if rpm < -max_rpm:
+        return -max_rpm
+    return rpm
 
+def forward_PID(Bot, f_distance = 600, kp = 3):
+    actual = min(Bot.get_range_image()[175: 180])
+    e = actual - f_distance
+    forward_v = saturation(kp * e)
+    return forward_v
 
 if __name__ == "__main__":
     Bot = HamBot(lidar_enabled=True, camera_enabled=False)
-    wheel_radius = 0.03
-    i = 0
+
     while True:
         forward_distance = min(Bot.get_range_image()[175: 180])
-        print("Forward distance: ", forward_distance)
-        i = i + 1
-        #forward_velocity = forward_PID(kp=3)
-        # too close
-        #if forward_distance :
+        forward_velocity = forward_PID(Bot, f_distance = 600, kp=3)
 
+        if forward_distance > 600:
+            Bot.set_left_motor_speed(forward_velocity)
+            Bot.set_right_motor_speed(forward_velocity)
+        else:
+            Bot.stop_motors()
+            break
 
 
