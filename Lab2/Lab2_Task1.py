58b0783a7f392aa3ba4d84c208d7ffc112397dee
dschagen <dschagen6@gmail.com>
2025-10-10 23:07:32 -0400

    Starting from scratch

--- Lab2/Lab2_Task1.py
+++ Lab2/Lab2_Task1.py
@@ -1,9 +1,9 @@
 from HamBot.src.robot_systems.robot import HamBot
 import math
 
-def saturation(Bot,v):
+#def saturation(Bot,v):
 
-def forward_PID(Bot, forward_distance = 0.6, kp = 3):
+#def forward_PID(Bot, forward_distance = 0.6, kp = 3):
 
 
 if __name__ == "__main__":
