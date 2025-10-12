43c5a7b024443217ca8d06ff502dfa74c5227791
mark5525 <halimmark5@gmail.com>
2025-10-12 19:01:52 -0400

    added some tolereance, and made it apply to the back and the front, in case the robot is closer than 1 meter

--- Lab2/Lab2_Task1.py
+++ Lab2/Lab2_Task1.py
@@ -2,13 +2,14 @@ from HamBot.src.robot_systems.robot import HamBot
 import math
 
 
-def saturation(Bot,rpm):
-    max_rpm = getattr(Bot, "max_motor_speed", 60)
+def saturation(bot, rpm):
+    max_rpm = getattr(bot, "max_motor_speed", 60)
     if rpm > max_rpm:
         return max_rpm
     if rpm < -max_rpm:
         return -max_rpm
     return rpm
+
 
 def forward_PID(Bot, f_distance = 600, kp = 0.2):
     scan = Bot.get_range_image()
@@ -20,7 +21,6 @@ def forward_PID(Bot, f_distance = 600, kp = 0.2):
     rpm_v = kp * e
     forward_v = saturation(Bot, rpm_v)
     return forward_v
-
 if __name__ == "__main__":
     Bot = HamBot(lidar_enabled=True, camera_enabled=False)
     Bot.max_motor_speed = 60
@@ -29,7 +29,10 @@ if __name__ == "__main__":
         forward_distance = min([a for a in Bot.get_range_image()[175:180] if a > 0] or [float("inf")])
         forward_velocity = forward_PID(Bot, f_distance = 600, kp=3)
 
-        if forward_distance > 600:
+        if forward_distance > 610:
+            Bot.set_left_motor_speed(forward_velocity)
+            Bot.set_right_motor_speed(forward_velocity)
+        elif forward_distance < 590:
             Bot.set_left_motor_speed(forward_velocity)
             Bot.set_right_motor_speed(forward_velocity)
         else:
