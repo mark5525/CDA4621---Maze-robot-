aba5310f6c85a5bafdcd51dc2616c24ed42cc7d1
dschagen <dschagen6@gmail.com>
2025-10-16 17:53:54 -0400

    Testing new code

--- Lab2/Lab2_Task1.py
+++ Lab2/Lab2_Task1.py
@@ -21,29 +21,27 @@ class Defintions():
         self.Time = 0
         self.Integral = 0.0
         self.Timestep = 0.032
-        self.Proportional = 0
-        self.Integral +=  0
-        self.Derivative = 0
-        self.Control = 0
         self.Saturated_Control = saturation(Bot, self.Control)
 
     def forward_PID(self, Bot, desired_distance):
-        Forward_PID_Values = Defintions()
         scan = Bot.get_range_image()
         window = [a for a in scan[175:180] if a > 0]
         if not window:
             return 0.0
-        Forward_PID_Values.MeasuredDistance = min(window)
-        Forward_PID_Values.DesiredDistance = desired_distance
-        Prev_error = Forward_PID_Values.Error
-        Forward_PID_Values.Proportional = Forward_PID_Values.K_p * Forward_PID_Values.Error
-        Forward_PID_Values.Integral += Forward_PID_Values.Error *self.Timestep
-        Forward_PID_Values.Derivative = (Forward_PID_Values.Error - Forward_PID_Values.Error_Previous) / Forward_PID_Values.Timestep
-        Forward_PID_Values.Control = (Forward_PID_Values.Proportional * Forward_PID_Values.Error) + (Forward_PID_Values.K_i *self.Integral) + (Forward_PID_Values.K_d * self.Derivative)
-        Sat_control = Forward_PID_Values.Saturated_Control
-        Forward_PID_Values.Error_Previous = Prev_error
 
-        return Sat_control
+        # Update measurements & error
+        self.MeasuredDistance = min(window)
+        self.DesiredDistance = desired_distance
+        self.Error = self.DesiredDistance - self.MeasuredDistance
+
+        # PID terms
+        self.Integral += self.Error * self.Timestep  # accumulate
+        derivative = (self.Error - self.Error_Previous) / self.Timestep
+
+        u = (self.K_p * self.Error) + (self.K_i * self.Integral) + (self.K_d * derivative)
+
+        self.Error_Previous = self.Error
+        return saturation(Bot, u)
 
 if __name__ == "__main__":
     Bot = HamBot(lidar_enabled=True, camera_enabled=False)
